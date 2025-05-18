c835d28df553bb6b249c02e23b9a2bb6c86142b1
Felipe Benitez <fbenitez@stratechcorp.com>
2025-05-18 17:31:14 -0400

    up translate

--- storefront/src/modules/home/components/hero/index.tsx
+++ storefront/src/modules/home/components/hero/index.tsx
@@ -5,7 +5,8 @@ const Hero = () => {
   return (
     <div className="h-[75vh] w-full relative bg-ui-bg-subtle">
       <div className="absolute inset-0 z-10 flex flex-col justify-center items-center text-center">
-        <img className="w-full h-full" src="/banner/banner.png" />
+        <img className="hidden md:block w-full h-full" src="/banner/banner.png" />
+        <img className="block md:hidden w-full h-full" src="/banner/banner_mobile.png" />
       </div>
     </div>
   )
