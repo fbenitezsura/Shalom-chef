50520d389ff7439080dfc02452be87792f824ca7
Felipe Benitez <fbenitez@stratechcorp.com>
2025-06-16 20:20:47 -0400

    up v

--- storefront/src/app/[countryCode]/(main)/order/confirmed/page.tsx
+++ storefront/src/app/[countryCode]/(main)/order/confirmed/page.tsx
@@ -1,47 +1,48 @@
 import { Metadata } from "next"
-import { notFound } from "next/navigation"
-import { placeOrder } from "@lib/data/cart";
 import OrderCompletedTemplate from "@modules/order/templates/order-completed-template"
 import { enrichLineItems } from "@lib/data/cart"
 import { retrieveOrder } from "@lib/data/orders"
-import type { HttpTypes } from "@medusajs/types"
-import { searchPaymentsByReference } from './../../../../../lib/mercadopago/getPayment';
-
-async function getOrder(id: string) {
-  const order = await retrieveOrder(id)
-  if (!order) return null
-
-  const items = await enrichLineItems(order.items, order.region_id!)
-  return { ...order, items } as unknown as HttpTypes.StoreOrder
-}
+import { HttpTypes } from "@medusajs/types"
 
 export const metadata: Metadata = {
-  title: "Order Confirmed",
-  description: "Your purchase was successful",
+  title: "Order",
+  description: "Your purchase is procesing...",
 }
 
 type Props = {
-  /** /order/confirmed?preference_id=<ORDER_ID> */
-  searchParams: { 
-    external_reference?: string, //payment session id
-    preference_id: string // reference id mercado pago
-   }
+  searchParams: {
+    external_reference?: string,
+    preference_id: string
+  }
+}
+
+async function getOrder(id: string) {
+  const order = await retrieveOrder(id)
+
+  if (!order) {
+    return
+  }
+
+  const enrichedItems = await enrichLineItems(order.items, order.region_id!)
+
+  return {
+    ...order,
+    items: enrichedItems,
+  } as unknown as HttpTypes.StoreOrder
 }
 
 export default async function OrderConfirmedPage({ searchParams }: Props) {
-  const sessionId = searchParams.external_reference;
-  const preference_id = searchParams.preference_id;
-  console.log("preference id usado", preference_id)
-  const payment = await searchPaymentsByReference(preference_id);
-  console.log("pago encontrado", payment);
-  if(payment.result > 0) {
-    await placeOrder();
-  } else {
+  const orderId : string = searchParams.external_reference || '';
 
+  const order = await getOrder(orderId);
+  if (!order) {
+    return (
+      <div className="my-10 h-[400px] w-full flex items-center justify-center " >
+        <p>No se ha realizado el pago</p>
+      </div>
+    )
   }
-  return (
-    <div className="my-10 h-[400px] w-full flex items-center justify-center " >
-      <p>No se ha realizado el pago</p>
-    </div>
-  )
+
+  return <OrderCompletedTemplate order={order} />
+
 }
